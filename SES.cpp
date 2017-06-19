b6675617877ff99a227c0f0a1a32b8e7795aa1ed
riya13 <riya_jindal@outlook.com>
2017-06-19 23:22:13 +0530

    Update SES.cpp

--- SES.cpp
+++ SES.cpp
@@ -51,7 +51,7 @@ int main() {
        // check for solution
        if ( xEnd >= N && yEnd >= M ) // Solution has been found!
        {
-           cout<<"Minimum number of moves required to convert string A into B is: "<< d << endl;
+           cout<<"Minimum edits required to convert string A into B is: "<< d << endl;
            flag = true;
            break;
        }
