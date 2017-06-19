a79fa33488436579b020f23a7be2264412dd2132
riya13 <riya_jindal@outlook.com>
2017-06-19 13:00:46 +0530

    Update SES.cpp

--- SES.cpp
+++ SES.cpp
@@ -1,10 +1,59 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
 
-  cout<<"Hello World";
-  
-  return 0;
+    string A, B;
+
+    cin>>A>>B;
+
+    int N = A.length();
+    int M = B.length();
+
+    int V[2*(N+M)];
+
+
+	  V[ 1 ] = 0;
+
+    int *p = & V[0];
+
+    for ( int d = 0 ; d <= N + M ; d++ )
+    {
+       for ( int k = -d ; k <= d ; k += 2 )
+       {
+       // down or right?
+       bool down = ( k == -d || ( k != d && p[ k - 1 ] < p[ k + 1 ] ) );
+
+       int kPrev = down ? k + 1 : k - 1;
+
+       // start point
+       int xStart = p[ kPrev ];
+       int yStart = xStart - kPrev;
+
+       // mid point
+       int xMid = down ? xStart : xStart + 1;
+       int yMid = xMid - k;
+
+       // end point
+       int xEnd = xMid;
+       int yEnd = yMid;
+
+       // follow diagonal
+       int snake = 0;
+       while ( xEnd < N && yEnd < M && A[ xEnd ] == B[ yEnd ] ) { xEnd++; yEnd++; snake++; }
+
+       // save end point
+       p[ k ] = xEnd;
+
+       // check for solution
+       if ( xEnd >= N && yEnd >= M ) // Solution has been found!
+        
+      }
+
+   }
+
+return 0;
   
 }
