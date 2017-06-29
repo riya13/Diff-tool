150b3694feb0b07fb860af7a0ce38fcaebb41cbb
riya13 <riya_jindal@outlook.com>
2017-06-29 12:40:39 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namesapce std;
+
+int main() {
+  
+  // number of colsof file A and file B
+  
+  int a , b;
+  
+  cout<<"Enter No. of Cols for File A and B";
+  cin>> a >> b;
+  
+  string A[a] , B[b];
+  
+  
+  
+  
+  int t = 2;
+  
+  // for 2 dimensional difference
+  
+  while(t--) {
+    
+    
+  
+  
+  
+  
+  }
+  
+
+
+
+return 0;
+}
